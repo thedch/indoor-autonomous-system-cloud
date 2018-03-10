996be9312ffa1ce39e7f02b19522a88e0457d64c
root <root@test-flask.c.dogwood-abbey-193600.internal>
2018-03-10 19:04:34 +0000

    added mapping functionality and renamed endpoints and buttons to reflect this. make killSwitch a toggle function with only one button

--- app/server.py
+++ app/server.py
@@ -5,6 +5,14 @@ from subprocess import call
 import datetime 
 import sqlite3
 from flask import g
+import ssl
+
+'''
+context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+context.load_cert_chain('/root/ca/intermediate/certs/ca-chain.cert.pem',
+        keyfile='/root/ca/intermediate/private/35.229.88.91.key.pem',
+        password="12345")
+'''
 
 app = Flask(__name__)
 
@@ -24,29 +32,34 @@ def close_connection(exception):
 def homepage():
     return app.send_static_file('index.html') 
 
-@app.route('/v1/robot_go')
-def robot_go():
-	#call(['echo', 'robot_go'])
-    call(['mosquitto_pub', '-t', 'robot/killSwitch', '-m', 'robot go'])
+@app.route('/v1/robot_start_mapping')
+def startMapping():
+    #call(['echo', 'robot_go'])
+    call(['mosquitto_pub', '-t', 'robot/mapping', '-m', 'robot start mapping'])
     return redirect("http://35.229.88.91", code=302)
 
-@app.route('/v1/robot_stop')
-def robot_stop():
-	#call(['echo', 'robot_stop'])
-    call(['mosquitto_pub', '-t', 'robot/killSwitch', '-m', 'robot stop'])
+@app.route('/v1/robot_stop_mapping')
+def stopMapping():
+    #call(['echo', 'robot_stop'])
+    call(['mosquitto_pub', '-t', 'robot/mapping', '-m', 'robot stop mapping'])
+    return redirect("http://35.229.88.91", code=302)
+
+@app.route('/v1/robot_toggle_killSwitch')
+def toggleKillSwitch():
+    call(['mosquitto_pub', '-t', 'robot/killSwitch', '-m', 'toggle'])
     return redirect("http://35.229.88.91", code=302)
 
 @app.route('/v1/robot_receive_map', methods=['POST'])
 def robot_receive_map():
     #database = get_db()
-   # imagefile = request.files.get('imagefile', '')
+    #imagefile = request.files.get('imagefile', '')
     #request.form['image'].save('/file.jpg')
     #filename_build = "map-" + datetime.date.today().strftime("%B-%d-%Y") 
     request.files['map.png'].save('./app/static/map.png') 
     
     #minspec return, no checking
     return "wassup" 
-#    print( request.form['name'])
+    #print(request.form['name'])
 
 
 '''
@@ -56,6 +69,7 @@ def updateDst():
 '''
 
 if __name__ == '__main__':
-	app.run(host="0.0.0.0", port=80)
+    context = ('/root/ians_cloud/server.cert', '/root/ians_cloud/server.key') 
+    app.run(host="0.0.0.0", port=80, ssl_context=context)
 
 
