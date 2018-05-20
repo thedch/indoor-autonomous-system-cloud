66d4677d051c127845bb06263f6a8fee1b7cbee8
cloud <kebding@gmail.com>
2018-05-20 17:56:36 +0000

    clean script of old environment setup lines

--- startserver.sh
+++ startserver.sh
@@ -5,8 +5,6 @@ if ! [ $(id -u) = 0 ]; then
 	exit 1
 fi
 
-source /root/indoor-autonomous-system-cloud/app/venv/bin/activate
 python /root/indoor-autonomous-system-cloud/app/server.py & 
-deactivate
 mosquitto &
 
